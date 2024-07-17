7aca7c63589eba47da05d1ee29d15075e3485a35
AlexKBADPMC <alex.knoll93@gmail.com>
2024-07-17 23:41:28 +0200

    Update AISettingsV3.hpp

--- OPCB/userconfig/VCOM_AI/AISettingsV3.hpp
+++ OPCB/userconfig/VCOM_AI/AISettingsV3.hpp
@@ -50,7 +50,7 @@ Vcm_Settings =
 	//VCM_AIDIFA = [['aimingAccuracy',0.15],['aimingShake',0.1],['aimingSpeed',0.25],['commanding',1],['courage',1],['endurance',1],['general',0.5],['reloadSpeed',1],['spotDistance',0.8],['spotTime',0.8]];
 		
 	//MEDIUM DIFFICULTY
-	VCM_AIDIFA = [['aimingAccuracy',0.2],['aimingShake',0.2],['aimingSpeed',0.6],['commanding',0.85],['courage',0.75],['general',1],['reloadSpeed',1],['spotDistance',0.8],['spotTime',0.8]];
+	VCM_AIDIFA = [['aimingAccuracy',0.2],['aimingShake',0.15],['aimingSpeed',0.5],['commanding',0.85],['courage',0.75],['general',0.8],['reloadSpeed',1],['spotDistance',0.2],['spotTime',0.8]];
 	
 	//HIGH DIFFICULTY
 	//VCM_AIDIFA = [['aimingAccuracy',0.4],['aimingShake',0.4],['aimingSpeed',0.45],['commanding',1],['courage',0.75],['endurance',1],['general',0.5],['reloadSpeed',1],['spotDistance',0.8],['spotTime',0.8]];
